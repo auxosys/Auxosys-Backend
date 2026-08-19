5ee08fee6b8043925a867825446e441251f4d10e
auxosys <auxosys@gmail.com>
2026-08-19 15:42:25 +0530

    chore: remove test scripts

--- test_crawler.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const crawlerService = require("./services/crawlerService");
-
-(async () => {
-  console.log("Starting crawl test...");
-  const result = await crawlerService.runCrawl();
-  console.log("Crawl result:", JSON.stringify(result, null, 2));
-  process.exit(0);
-})();
