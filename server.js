af8b2f78ea8f0aa02383ba0d32658574e3c952dc
auxosys <auxosys@gmail.com>
2026-09-09 17:52:02 +0530

    fix(api): increase express body parser payload limit to 50mb for PDF email attachments

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.use(cors({
   },
   credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
